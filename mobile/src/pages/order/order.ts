38fd4096319ae2c2ea1b074bfae93d4cd516d633
gaborfekete85 <fgabiweb@gmail.com>
2017-05-25 12:20:41 +0200

    Add items to basket

--- mobile/src/pages/order/order.ts
+++ mobile/src/pages/order/order.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
-// import { NavController } from 'ionic-angular';
 import { PlaceService } from '../../providers/PlaceService';
 import { Observable } from "rxjs/Observable";
 import {Place} from "../../domain/Place";
 import {MenuService} from "../../providers/MenuService";
 import {Category} from "../../domain/menu/Category";
 import {Item} from "../../domain/menu/Item";
-// import { Place } from "../../domain/Place";
 import { Storage } from '@ionic/storage';
 import {Basket} from "../../domain/basket/Basket";
 import {BasketItem} from "../../domain/basket/BasketItem";
@@ -61,20 +59,34 @@ export class OrderPage {
   public order( item : Item) {
     this.storage.get('basket').then((response) => {
       let basket : Basket = JSON.parse(response);
-      console.log(basket.items.length);
-      basket.items.filter(x => x.item._id === item._id)
-        .map(x => function(x) {
-          console.log(x);
-          // if(x.orders && x.orders > 1) {
-          //   x.orders++;
-          // } else {
-          //   basket.items.push(new BasketItem(item, 1));
-          // }
-        });
-      this.basketItemsCount = this.basketItemsCount++;
+      let index = this.findWithAttr(basket.items, '_id', item._id);
+      if( index == -1) {
+        basket.items.push(new BasketItem(item, 1));
+      } else {
+        basket.items[index]["item"]["orders"] = basket.items[index]["item"]["orders"] + 1;
+      }
+      this.basketItemsCount = this.basketItemsCount+1;
       this.storage.set('basket', JSON.stringify(basket));
+
+      // basket.items.filter(x => x.item._id === item._id)
+      //   .map(x => function(x) {
+      //     itemInArray = true;
+      //     this.basketItemsCount = this.basketItemsCount+1;
+      //     x.orders = x.orders + 1;
+      //     this.storage.set('basket', JSON.stringify(basket));
+      //   });
     });
   }
+
+  private findWithAttr(array, attr, value) {
+    for(var i = 0; i < array.length; i += 1) {
+      if(array[i]["item"][attr] === value) {
+        return i;
+      }
+    }
+  return -1;
+  }
+
 
   public getPlaces() {
     this.placeService.getPlaces().subscribe(response => {
