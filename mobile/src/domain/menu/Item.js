6f1b897b643e08ef0f77151be002a8e4a0e01280
gaborfekete85 <fgabiweb@gmail.com>
2017-05-26 21:12:55 +0200

    Domain objects

---
+++ mobile/src/domain/menu/Item.js
@@ -0,0 +1,10 @@
+"use strict";
+exports.__esModule = true;
+var Item = (function () {
+    function Item() {
+        this.orders = 0;
+    }
+    return Item;
+}());
+exports.Item = Item;
+;
---
+++ mobile/src/domain/menu/Order.js
@@ -0,0 +1,10 @@
+"use strict";
+exports.__esModule = true;
+var Order = (function () {
+    function Order() {
+        this.orders = 0;
+    }
+    return Order;
+}());
+exports.Order = Order;
+;
